f8ec683e75f86122309ad57d00e236ed7319a296
Jonathan Kofi Donkor <jkd6735@gmail.com>
2023-09-14 14:08:04 +0000

    first commit

---
+++ css-grids/src/App.jsx
@@ -0,0 +1,27 @@
+import {useState} from "react";
+import "./App.css";
+
+function App() {
+	return (
+		<>
+			<div className="wrapper">
+				<div className="one">1</div>
+
+				<div className="two">2</div>
+				<div className="five">5</div>
+
+				<div className="three">3</div>
+				<div className="four">4</div>
+
+				<div className="six">6</div>
+
+				<div className="seven">7</div>
+
+				<div className="eight">8</div>
+				<div className="nine">9</div>
+			</div>
+		</>
+	);
+}
+
+export default App;
