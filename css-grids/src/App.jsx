8471cc7212120cae7944f9e2efa81ab93179a268
Jonathan Kofi Donkor <jkd6735@gmail.com>
2023-09-15 11:08:56 +0000

    update

--- css-grids/src/App.jsx
+++ css-grids/src/App.jsx
@@ -20,6 +20,34 @@ function App() {
 				<div className="eight">8</div>
 				<div className="nine">9</div>
 			</div>
+
+			<div className="footer">
+				<div className="first">
+					<h2>Funiro</h2>
+					<p>This is our Funiro page</p>
+				</div>
+				<div className="second">
+					<h2>Links</h2>
+					<p>Home</p>
+					<p>Home</p>
+					<p>Home</p>
+					<p>Home</p>
+				</div>
+				<div className="third">
+					<h2>Help</h2>
+					<p>Home</p>
+					<p>Home</p>
+					<p>Home</p>
+					<p>Home</p>
+				</div>
+				<div className="fourth">
+					<h2>Newsletter</h2>
+					<p>Home</p>
+					<p>Home</p>
+					<p>Home</p>
+					<p>Home</p>
+				</div>
+			</div>
 		</>
 	);
 }
